cbdf2c6bedd5a08e75d809c4bdcbdcfcf554d1f7
royc0003 <royceang96@gmail.com>
2019-11-08 04:01:15 +0800

    <Added BaseLine Features> May need to discuss on the showtimes and how it should be linked

--- MovieManager/Movie.java
+++ MovieManager/Movie.java
@@ -1,23 +1,52 @@
-public class Movie implements Serialization{
+import java.util.ArrayList;
+
+public class Movie implements Serialization, Comparable<Movie>{
     private int movieID;
     private String movieTitle;
-    private int movieRating;
-    private int movieAgeRating;
+    private ArrayList<Integer> listOfRatings = new ArrayList<>();
     private String showingStatus;
+    private int movieAgeRating;
+    private int averageRating; // main one
+    private int duration;
+    private ArrayList<String> listOfReviews = new ArrayList<String>(); // array list of reviews
 
-    public Movie(int movieID, String movieTitle, int movieRating,int movieAgeRating, String showingStatus){
-        this.movieID =movieID;
-        this.movieTitle = movieTitle;
-        this.movieRating = movieRating;
-        this.movieAgeRating =movieAgeRating;
+    public Movie(int iD, String title, int ageRating, String showingStatus, int duration)
+    {
+        this.movieID = iD;
+        this.movieTitle = title;
+        this.movieAgeRating = ageRating;
         this.showingStatus = showingStatus;
+        this.duration = duration;
     }
-public void setMovieID(int movieID){
+
+    public void addToAverageRating(int e)  //Reviews/Ratings Manager Class will use this function to add ratings to a particular movie
+    {
+        listOfRatings.add(e);
+
+        int totalRatingSum = 0;
+
+        for(int i = 0; i< listOfRatings.size(); i++)
+        {
+            totalRatingSum = totalRatingSum + listOfRatings.get(i);
+        }
+
+        int averageRating = totalRatingSum / listOfRatings.size();
+
+        this.averageRating = averageRating;
+    }
+
+    public int getAverageRating(){
+        return this.movieAgeRating;
+    }
+
+
+    public void setMovieID(int movieID){
     this.movieID = movieID;
 }
 public int getMovieID(){
         return movieID;
 }
+
 
 public void setMovieTitle(String movieTitle){
         this.movieTitle = movieTitle;
@@ -25,33 +54,60 @@ public void setMovieTitle(String movieTitle){
 public String getMovieTitle(){
        return movieTitle;
 }
-public void setMovieRating(int movieRating){
-        this.movieRating = movieRating;
+
+
+
+public void setMovieDuration(int duration){
+        this.duration = duration;
 }
-public int getMovieRating(){
-    return movieAgeRating;
+public int getMovieDuration(){
+        return this.duration;
 }
 
 
 public void setMovieAgeRating(int movieAgeRating){
         this.movieAgeRating = movieAgeRating;
-}
+    }
 public int getMovieAgeRating(){
     return movieAgeRating;
 }
 
 
-public void setShowingStatus(String showingStatus){
+public void setMovieShowingStatus(String showingStatus){
         this.showingStatus = showingStatus;
 }
-public String getShowingStatus(){
+public String getMovieShowingStatus(){
     return showingStatus;
 }
-public String[] toCsv(){
-    String[] csv = {Integer.toString(this.movieID), this.movieTitle, Integer.toString(this.movieRating),
-            Integer.toString(movieAgeRating), this.showingStatus
-    };
-    return csv;
+
+public void displayMovieAndReviews()
+    {
+        System.out.println(movieTitle);
+        for(int i = 0; i< listOfReviews.size(); i++)
+        {
+            System.out.println(listOfReviews.get(i));
+        }
+
+    }
+
+    public void addReview(String review)
+    {
+        listOfReviews.add(review);
+    }
+
+
+    @Override
+    public int compareTo(Movie m)
+    {
+        return m.getAverageRating() - averageRating;
+    }
+
+
+    public String[] toCsv(){
+        String[] csv = {Integer.toString(this.movieID), this.movieTitle, this.showingStatus, Integer.toString(this.movieAgeRating),
+                Integer.toString(movieAgeRating)
+        };
+        return csv;
     }
 }
 
--- MovieManager/MovieManager.java
+++ MovieManager/MovieManager.java
@@ -1,78 +1,116 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Scanner;
 
 public class MovieManager {
     private MovieDisplay displayMovie;
-    private ArrayList<MovieCreator> listOfMovieCreator;
+    private ArrayList<MovieAndShowtimes> listOfMovieAndShowtimes;
 
-    public MovieManager(MovieDisplay displayMovie, ArrayList<MovieCreator> listOfMovieCreator){
+    public MovieManager(MovieDisplay displayMovie, ArrayList<MovieAndShowtimes> listOfMovieAndShowtimes){
         this.displayMovie = displayMovie;
-        this.listOfMovieCreator = listOfMovieCreator;
+        this.listOfMovieAndShowtimes = listOfMovieAndShowtimes;
     }
-    public void printDetails(ArrayList<MovieCreator> listOfMovieCreator){
-        for(MovieCreator x : listOfMovieCreator){
-            displayMovie.printDetails(x.getMovie().getMovieID(),x.getMovie().getMovieTitle(), x.getMovie().getMovieRating(), x.getMovie().getMovieAgeRating(), x.getMovie().getShowingStatus());
+    public void printDetails(ArrayList<MovieAndShowtimes> listOfMovieAndShowtimes){
+        for(MovieAndShowtimes x : listOfMovieAndShowtimes){
+            displayMovie.printDetails(x.getMovieID(), x.getMovieTitle(), x.getMovieAgeRating(),x.getMovieAverageRating(), x.getMovieDuration(), x.getMovieShowingStatus()
+            );
         };
     }
-    public void createMovieCreator(){
+
+    //--------------MANAGES WITH CREATION OF MOVIES-------------------------------------------------------------------------------------------------------
+
+    public void createMovieCreator(){ //createMovieShowTimes objects to be added in a list
         Scanner sc = new Scanner(System.in);
-        System.out.println("Insert number of creators");
+        System.out.println("Insert number of MovieAndShowtimes to create:");
         int i = sc.nextInt();
         for(int x= 0; x< i; x++){
-            listOfMovieCreator.add(new MovieCreator());
+            listOfMovieAndShowtimes.add(new MovieAndShowtimes());
         }
+        createMovie();;// adds movie to every single MovieShhowTimes objects created
     }
 
-    public void createMovieShowtimes(){ // new function
-        createMovie();
-        for(MovieCreator n: listOfMovieCreator){ // we need to check for every title
-            System.out.println("You are referring to this movie: "+ n.getMovieTitle());
-            createShowTimes();
+
+    private void createMovie(){ //creates new movies
+
+        for(MovieAndShowtimes n : listOfMovieAndShowtimes){
+            n.createMovie();
         }
+
     }
-    public void removeMovie(int index) {
-        for(MovieCreator n: listOfMovieCreator){
-            if(n.getMovieID() == index){
-                listOfMovieCreator.remove(index);
+    //--------------MANAGES WITH REMOVAL OF MOVIES-------------------------------------------------------------------------------------------------------
+    public void removeMovie(int MovieID) { // removes an object from a list of created Movies
+        for(MovieAndShowtimes n: listOfMovieAndShowtimes){
+            if(n.getMovieID() == MovieID){
+                listOfMovieAndShowtimes.remove(n);
                 break;
             }
         }
     }
+    //--------------MANAGES WITH LISTING OF DETAILS------------------------------------------------------------------------------------------------------
+// return list of movieIDs done
+    // prints list of movieIDs done
+    // returns list of movieObjects
+    // gets a single movie from the list of movieObjects --
+public void printGlobalListOfMovieIDs(){
+        System.out.println("******************************************************************");
+        for(MovieAndShowtimes n: listOfMovieAndShowtimes){
+            System.out.println("Movie ID: "+ n.getMovieID() +", Movie Title: "+n.getMovieTitle());
+        }
+    System.out.println("******************************************************************");
+}
+public ArrayList<Integer> getGlobalListOfMovieIDs(){
+        ArrayList<Integer> listOfMovieIDs = new ArrayList<Integer>();
+        for(MovieAndShowtimes n: listOfMovieAndShowtimes){
+            listOfMovieIDs.add(n.getMovieID());
+        }
+        return listOfMovieIDs;
+}
+public ArrayList<Movie> getGlobalListOfMovieObjects(){
+        ArrayList<Movie> arrayListOfMovieOBjects = new ArrayList<Movie>();
+        for(MovieAndShowtimes n: listOfMovieAndShowtimes){
+            arrayListOfMovieOBjects.add(n.getMovie());
+        }
+        return arrayListOfMovieOBjects;
+}
+public Movie getMovie(int MovieID){
+    Movie x = null;
+        for(MovieAndShowtimes n: listOfMovieAndShowtimes){
+            if(n.getMovieID() == MovieID)
+                x= n.getMovie();
+            break;
+        }
+    return x;
+}
 
 
-    private void createMovie(){
-        for(MovieCreator n : listOfMovieCreator){
-            n.createMovie();
+    //--------------MANAGES WITH SHOWTIMES------------------------------------------------------------------------------------------------------
+
+
+    public void createMovieShowtimes(){ // This is to be made dynamic
+        for(MovieAndShowtimes n: listOfMovieAndShowtimes){ // we need to check for every title
+            System.out.println("You are referring to this movie: "+ n.getMovieTitle());
+            createShowTimes();
         }
     }
 
-    private void createShowTimes(){
+//createSingleShowTime
+    //createMultipleShowTime
+    private void createShowTimes(){ // creates  Showtimes
         Scanner sc = new Scanner(System.in);
-        for(MovieCreator n: listOfMovieCreator){
+        for(MovieAndShowtimes n: listOfMovieAndShowtimes){
             //Date showBegins =; // remember to update this
             //Date showEnds =  ; // remember to update this
             //n.createShowTime(Date showBegins, Date showEnds);
         }
     }
 
-    public void getMovie(int index) { // to poinder upon remeber to input size
-        for (MovieCreator n : listOfMovieCreator) {
-            if (n.getMovieID() == index) {
-                return ;
-            }
-        }
-    }
 
-    public void getShowLIst(int index){ // remenber to input size
-        for(MovieCreator n: listOfMovieCreator){
-            if(n.getMovieID() == index){
+    public void getShowList(int MovieID){ // remenber to input size
+        for(MovieAndShowtimes n: listOfMovieAndShowtimes){
+            if(n.getMovieID() == MovieID){
                 //return n.getShowList();
             }
         }
     }
-
 
 
 
